1c382fe726812538ebc6fd98174f833f63f917b9
王雄飞 <23335049@qq.com>
2019-05-17 17:07:08 +0800

    log tools for jenkins

---
+++ vars/log.groovy
@@ -0,0 +1,14 @@
+/**
+ * class functional description
+ *
+ * @author wang.xiongfei
+ * @since 2019-05-17
+ * @version 1.0.0
+ */
+def info(message) {
+    echo "INFO: ${message}"
+}
+
+def waring(message) {
+    echo "WARNING: ${message}"
+}
